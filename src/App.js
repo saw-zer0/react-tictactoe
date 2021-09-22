23b308c50cecaadafee2fdc246c51340cfd19c0b
Sanil Shakya <sanil@gurzu.net>
2021-09-22 12:54:49 +0545

    added moves History feature and game reset function, game board will be unclickable after game ends until reset

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react"
 import GameBoard from "./components/GameBoard"
 import MovesHistory from "./components/MovesHistory"
+import { Button } from "@mui/material"
 
 
 class App extends Component {
@@ -19,22 +20,30 @@ class App extends Component {
       [0, 1, 2], [3, 4, 5], [6, 7, 8],
       [0, 3, 6], [1, 4, 7], [2, 5, 8],
       [0, 4, 8], [2, 4, 6]];
+    this.endGame = false;
+    this.initialState = JSON.parse(JSON.stringify(this.state))
   }
+
+
 
   componentDidUpdate(prevProps) {
     this.winCheck()
   }
 
   updateGameBoard = (e, cellId) => {
-    if (this.state.currentBoard[cellId]) {
+    if (this.state.currentBoard[cellId] || this.endGame) {
       return;
     }
     let board = JSON.parse(JSON.stringify(this.state.currentBoard))
     board[cellId] = this.state.currentPlayer
     
+    let movesHistory = JSON.parse(JSON.stringify(this.state.movesHistory))
+    movesHistory.push({cellId, player:this.state.currentPlayer})
+    console.log(movesHistory)
     this.changePlayer()
     this.setState({
       currentBoard: board,
+      movesHistory: movesHistory,
       counter: this.state.counter + 1
     })
   }
@@ -59,7 +68,12 @@ class App extends Component {
   }
 
   displayMessage = (win) => {
-    win ? alert(`player ${this.state.prevPlayer} Wins`) : alert("Its a draw..good game..")
+    win ? alert(`player ${this.state.prevPlayer} Wins`) : alert("Its a draw..good game..");
+    this.endGame = true;
+  }
+
+  resetGame = () => {
+    this.setState(JSON.parse(JSON.stringify(this.initialState)))
   }
 
   render() {
@@ -71,7 +85,14 @@ class App extends Component {
           currentPlayer={this.state.currentPlayer}
           updateGameBoard={this.updateGameBoard}
         />
-        <MovesHistory />
+        <MovesHistory
+          movesHistory={this.state.movesHistory}
+        />
+        <div>
+          <Button variant="contained" onClick={this.resetGame}>
+            RESET
+          </Button>
+        </div>
       </div>
     )
   }
--- src/components/MovesHistory.js
+++ src/components/MovesHistory.js
@@ -1,10 +1,16 @@
-import React, {Component} from "react"
+import React, { Component } from "react"
 
-export default class GameBoard extends Component{
+export default class GameBoard extends Component {
 
-    render(){
-        return(
-            <h1>MovesHistory</h1>
+    render() {
+        const {movesHistory} = this.props
+        return (
+            <div>
+                <h1>MovesHistory</h1>
+                <ul>
+                    {movesHistory.map( move => <li key={move.cellId}>{`player: ${move.player}, placed on ${move.cellId + 1}`}</li>)}
+                </ul>
+            </div>
         )
     }
 }
